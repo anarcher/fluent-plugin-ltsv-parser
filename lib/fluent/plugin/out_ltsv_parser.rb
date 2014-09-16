c0bf4c899636cc4a52edc72d72244eeadd58e65b
msshin <msshin@enswer.net>
2014-09-16 16:00:24 +0900

    Add filter method and filter_in config_param

--- lib/fluent/plugin/out_ltsv_parser.rb
+++ lib/fluent/plugin/out_ltsv_parser.rb
@@ -5,6 +5,7 @@ class Fluent::ParserOutput < Fluent::Output
     config_param :tag, :string, :default => nil
     config_param :reserve_data, :bool, :default => false
     config_param :key_name, :string
+    config_param :filter_in, :string
 
     attr_reader :parser
 
@@ -24,13 +25,14 @@ class Fluent::ParserOutput < Fluent::Output
             @key_name = @key_name[1..-1].to_sym
         end
         @parser = FluentExt::LTSVParser.new(log())
+        @filter_in = @filter_in.split(",").map(&:strip).select{ |e| e != "" }
     end
 
     def emit(tag,es,chain)
         tag = @tag || tag
         es.each do |time,record|
             raw_value = record[@key_name]
-            t,values = raw_value ? parse(raw_value) : [nil,nil]
+            t,values = raw_value ? filter(parse(raw_value)) : [nil,nil]
             t ||= time
 
             r = @reserve_data ? record.merge(values) : values 
@@ -43,6 +45,13 @@ class Fluent::ParserOutput < Fluent::Output
     end
 
     private
+
+    def filter(record)
+        if @filter_in.length > 0 then
+            record.select{ |x| @filter_in.include? x } 
+        end
+        record
+    end
 
     def parse(string)
         return @parser.parse(string) 
