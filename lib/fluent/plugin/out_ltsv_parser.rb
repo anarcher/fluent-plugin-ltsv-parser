5d2152444b3f56736dcc00a86af9564d93976829
msshin <msshin@enswer.net>
2014-09-24 17:38:38 +0900

    0.0.9 Add config param named transform_values  (LTSV Format using commna field delim.)

--- lib/fluent/plugin/out_ltsv_parser.rb
+++ lib/fluent/plugin/out_ltsv_parser.rb
@@ -7,6 +7,7 @@ class Fluent::ParserOutput < Fluent::Output
     config_param :filter_in, :string , :default => ""
     config_param :add_prefix, :string ,:default => nil
     config_param :min_field, :integer , :default => nil
+    config_param :transform_values,:string,:default => nil
 
     def initialize
         super
@@ -20,10 +21,16 @@ class Fluent::ParserOutput < Fluent::Output
 
     def configure(conf)
         super
+
         if @key_name[0] == ":" 
             @key_name = @key_name[1..-1].to_sym
         end
+
         @filter_in = @filter_in.split(",").map(&:strip).select{ |e| e != "" }
+
+        if @transform_values then
+            @transform_values = Hash[@transform_values.split(",").map{|p| p.split(":",2)}]
+        end
     end
 
     def emit(tag,es,chain)
@@ -33,7 +40,7 @@ class Fluent::ParserOutput < Fluent::Output
         end
         es.each do |time,record|
             raw_value = record[@key_name]
-            values = raw_value ? filter(parse(raw_value)) : nil
+            values = raw_value ? transform_values(filter(parse(raw_value))) : nil
 
             r = @reserve_data ? record.merge(values) : values 
             if r
@@ -63,6 +70,17 @@ class Fluent::ParserOutput < Fluent::Output
         end
 
         return _record
+    end
+
+    def transform_values(record)
+        return record if @transform_values.nil?
+
+        @transform_values.each do |key,value|
+            field_name,field_value = key.split("=")
+            next if record[field_name].nil? or field_name.nil? or field_value.nil? 
+            record[field_name] = value if field_value == record[field_name]
+        end
+        return record
     end
 
     def parse(text)
